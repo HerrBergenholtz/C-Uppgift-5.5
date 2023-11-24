d867fd588b5550022b647d0ef58ef2e3a5514a6b
HerrBergenholtz <zbdv8800@gmail.com>
2023-11-24 10:08:57 +0100

    Add project files.

---
+++ C Uppgift 5.5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] magiskaSvar = {
+            "Absolut!", "Fårga Ameen", "Fråga David", "Detta är INTE semantiskt korrekt",
+            "Detta är inte en bra idé", "Dålig idé", "NEJ", "JA", "Kanske", "Vad skulle Ameen tycka?"
+        };
+        bool running = true;
+        
+        try
+        {
+            while (running)
+            {
+                Console.WriteLine("Ställ en fråga till den magiska 8-bollen,,, eller skriv \"a\" för att Avsluta");
+                string svar = Console.ReadLine();
+            
+                if (svar == "a")
+                {
+                    running = false;
+                }
+                else
+                {
+                    Random slump = new Random();
+                    Console.WriteLine(magiskaSvar[slump.Next(10)]);
+                    Console.WriteLine("");
+                }
+            }
+            Console.WriteLine("Välkommen åter");
+            Console.ReadKey();
+        }
+
+        catch
+        {
+            Console.WriteLine("8-Bollen accepterar inte din input, vänligen försök igen");
+            Main();
+        }
+    }
+}
